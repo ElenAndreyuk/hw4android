0dfff11f2fd0f31688b5dcd9b4ac6a62d79f0671
Elena <femtochka@yahoo.com>
2024-02-21 15:55:09 +0300

    main

--- app/src/main/java/ru/elenandreyuk/hw4/MainActivity.kt
+++ app/src/main/java/ru/elenandreyuk/hw4/MainActivity.kt
@@ -2,56 +2,74 @@ package ru.elenandreyuk.hw4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.isNotEmpty
+import androidx.core.widget.doAfterTextChanged
 import ru.elenandreyuk.hw4.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-//        val username = binding.username
-//        username.setOnTouchListener { v, event ->
-//            val DRAWABLE_RIGHT = 2
-//
-//            if (event.action == MotionEvent.ACTION_UP) {
-//                if (event.rawX >= (username.right - username.compoundDrawables[DRAWABLE_RIGHT].bounds.width())) {
-//                    username.setText("")
-//                    return@setOnTouchListener true
-//                }
-//            }
-//            false
-//        }
-
-//        val checkedRadioButtonId = radioGroup.checkedRadioButtonId // Returns View.NO_ID if nothing is checked.
-//        radioGroup.setOnCheckedChangeListener { group, checkedId ->
-//            // Responds to child RadioButton checked/unchecked
-//        }
-//
-//// To check a radio button
-//        radioButton.isChecked = true
-//
-//// To listen for a radio button's checked/unchecked state changes
-//        radioButton.setOnCheckedChangeListener { buttonView, isChecked
-//            // Responds to radio button being checked/unchecked
-//        }
-
-//        // To listen for a switch's checked/unchecked state changes
-//        binding.switch_notification.setOnCheckedChangeListener { buttonView, isChecked
-//            // Responds to switch being checked/unchecked
-//        }
-
-//        // Set error.
-//        checkbox.errorShown = true
-//
-//// Optional listener:
-//        checkbox.addOnErrorChangedListener { checkBox, errorShown ->
-//            // Responds to when the checkbox enters/leaves error state
-//        }
-//
-//// To set a custom accessibility label:
-//        checkbox.errorAccessibilityLabel = "Error: custom error announcement."
+        initActivityState()
     }
+
+    private fun checkConditions(): Boolean {
+        val conditionNameField = binding.username.editText.toString().length in 1..40
+        val conditionPhoneField = binding.userPhone.isNotEmpty()
+        val conditionGenderIsChecked = binding.gender.checkedRadioButtonId!=-1
+
+        val conditionNotificationSwitch = (binding.switchNotification.isChecked &&
+                (binding.authorizationNotification.isChecked ||
+                binding.newsNotification.isChecked))
+        return conditionGenderIsChecked && conditionNameField
+                && conditionPhoneField && conditionNotificationSwitch
+    }
+
+    private fun changeSaveButtonState() {
+        if (checkConditions()) {
+            binding.buttonSave.isEnabled = true
+        } else {
+            binding.buttonSave.isEnabled = false
+        }
+    }
+
+    private fun initActivityState() {
+        val numberOfPoints = Random.nextInt(101)
+        binding.linearProgress.secondaryProgress = numberOfPoints
+        binding.countOfPoints.text = String.format(getString(R.string.points_of_number, numberOfPoints))
+        binding.newsNotification.setOnCheckedChangeListener { _, _ -> changeSaveButtonState()}
+        binding.authorizationNotification.setOnCheckedChangeListener { _, _ -> changeSaveButtonState()}
+        binding.textInputName.doAfterTextChanged { changeSaveButtonState() }
+        binding.textInputPhone.doAfterTextChanged {changeSaveButtonState()
+        }
+
+
+        binding.switchNotification.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.authorizationNotification.isEnabled = true
+                binding.newsNotification.isEnabled = true
+                changeSaveButtonState()
+            } else {
+                binding.authorizationNotification.isEnabled = !binding.authorizationNotification.isEnabled
+                binding.newsNotification.isEnabled = !binding.newsNotification.isEnabled
+                binding.authorizationNotification.isChecked = false
+                binding.newsNotification.isChecked = false
+                changeSaveButtonState()
+            }
+        }
+
+
+        binding.gender.setOnCheckedChangeListener { _, _ ->
+            changeSaveButtonState()
+        }
+
+        binding.buttonSave.setOnClickListener {
+            Toast.makeText(this, R.string.toast_text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
